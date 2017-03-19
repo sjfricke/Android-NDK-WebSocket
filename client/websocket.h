1eb541490dd219cba27a2e9d7af99bf6e83845e8
sjfricke <sjfricke@wisc.edu>
2017-03-19 09:46:47 -0500

    websocket interface outlined

---
+++ client/websocket.h
@@ -0,0 +1,44 @@
+#ifndef ANDROID_NDK_WEBSOCKET_H
+#define ANDROID_NDK_WEBSOCKET_H
+
+#include <string>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define MAX_MESSAGE_BUFFER
+
+class WebSocket {
+
+  public {
+    
+    // Constructor and destructor
+    WebSocket();
+    ~WebSocket();
+
+    // used to setup and connect to server
+    // returns 0 on success
+    int connect(std::string ip, int port);
+
+    // sends message to all other users online
+    // returns 0 on success
+    int broadcast(int key, int option, std::string message);
+  }
+
+  private {
+
+    struct sockaddr_in server_addr; // socket struct object
+    int socket_fd;                  // holds socket file discriptor
+    std::string msg_buffer_in;      // incoming messages
+    std::string msg_buffer_out;     // outgoing messages
+    pthread_t receive_thread;       // thread to block for messages
+    
+  }
+  
+}
+
+#endif // ANDROID_NDK_WEBSOCKET_H
